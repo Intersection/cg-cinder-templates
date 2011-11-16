bddcd0453f70a94309bc53a0b63840f1717ab8c3
Toby Joe Boudreaux <tj@tobyjoe.com>
2011-11-16 16:27:49 -0500

    Added OpenCV templates for Kinect and Cam

---
+++ Project Templates/Cinder/Cinder OpenCV and Kinect Application.xctemplate/include/Constants.h
@@ -0,0 +1,10 @@
+#pragma once
+#include "cinder/Color.h"
+
+static const float kWindowWidth		= 640.0f;
+static const float kWindowHeight	= 480.0f;
+
+static const float kCaptureWidth	= 640.0f;
+static const float kCaptureHeight	= 480.0f;
+
+static const ci::Color kClearColor	= ci::Color(0.0f, 0.0f, 0.0f);
